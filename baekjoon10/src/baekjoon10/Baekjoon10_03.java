bb959d8529c5301f9255eb134e2135085d7417bb
Rege-97 <cogus1027@gmail.com>
2024-12-31 18:13:14 +0900

    Test : 2024.12.31 -1

---
+++ baekjoon10/src/baekjoon10/Baekjoon10_03.java
@@ -0,0 +1,38 @@
+package baekjoon10;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon10_03 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int arr[][] = new int[4][2];
+
+		for (int i = 0; i < 3; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		if (arr[0][0] == arr[1][0]) {
+			arr[3][0] = arr[2][0];
+		} else if (arr[0][0] == arr[2][0]) {
+			arr[3][0] = arr[1][0];
+		} else {
+			arr[3][0] = arr[0][0];
+		}
+		
+		if (arr[0][1] == arr[1][1]) {
+			arr[3][1] = arr[2][1];
+		} else if (arr[0][1] == arr[2][1]) {
+			arr[3][1] = arr[1][1];
+		} else {
+			arr[3][1] = arr[0][1];
+		}
+		
+		System.out.println(arr[3][0]+" "+arr[3][1]);
+
+	}
+}
